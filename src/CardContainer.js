63a3bcefff9404c306615091cbb04887989c0f50
Eric Weissman <eric.weissman21@gmail.com>
2019-01-05 10:06:12 -0700

    Add structure for card container

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,10 +1,28 @@
-// import React, { Component } from 'react'
-// import './styles/Main.scss'
+import React, { Component } from 'react'
+import QuestionCard from './QuestionCard'
+import './styles/Main.scss'
 
-// class CardContainer extends Component {
-//   render() {
-//     return()
-//   }
-// }
+class CardContainer extends Component {
+  render() {
+    let {questions} = this.props.questions
+    return (
+      <div className="card-container">
+        {
+          questions.map((question) => {
+            return (
+              <QuestionCard 
+                key={question.id}
+                question={question.question}
+                answers={question.answers}
+                correct_answer={questions.correct_answer}
+                concept={questions.concept}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
 
-// export default CardContainer;
+export default CardContainer;
