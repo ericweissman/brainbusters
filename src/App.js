814f014fe4634464c9ff5fd85927336f7b88e295
Eric Weissman <eric.weissman21@gmail.com>
2019-01-04 17:03:43 -0700

    Adds Card container component skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import Header from './Header'
+import Header from './Header';
+import CardContainer from './CardContainer';
 import './styles/Main.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      questions: []
+    }
+  }
+  componentDidMount() {
+    fetch("http://memoize-datasets.herokuapp.com/api/v1/ewQuestions")
+      .then(results => results.json())
+      .then((result) => {
+        this.setState({
+          questions: result.ewQuestions
+        })
+      })
+      .catch(err => {
+        this.setState({ error: err })
+      })
+    }
+
   render() {
     return (
       <div className="App">
         <Header />
+        <CardContainer />
       </div>
     );
   }
---
+++ src/CardContainer.js
@@ -0,0 +1,10 @@
+// import React, { Component } from 'react'
+// import './styles/Main.scss'
+
+// class CardContainer extends Component {
+//   render() {
+//     return()
+//   }
+// }
+
+// export default CardContainer;
